47c3a30132ccccbfbb476f5a50baa499b4476b95
simonblaue <sblaue@gmx.de>
2022-06-30 17:14:21 +0200

    stability zeug ?

--- ModelLib.py
+++ ModelLib.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
+import scipy.optimize as opt
 
 class SIRModels:
     def __init__(self, gamma=0.1, beta=0.5, mue=0, nue=1/100):
@@ -14,10 +15,11 @@ class SIRModels:
         self.s = 0.3 # Amplitude of seasonal forcing
         self.omega = 2*np.pi/360 #Frequency of yearly seasonsl variation
         self.T = 60 # mittlerer Erinnerungszeit
-        self.P = lambda state: self.p_base+(1-self.p_base)/self.p_cap*self.epsilon*np.log(1+np.exp(1/self.epsilon*(self.p_cap-state[4])))
+        self.P = lambda H: self.p_base+(1-self.p_base)/self.p_cap*self.epsilon*np.log(1+np.exp(1/self.epsilon*(self.p_cap-H)))
         self.Gamma = lambda t : 1+ self.s*np.cos(self.omega*t)
-        self.dP = lambda state : (((1-self.p_base)/self.p_cap)*self.epsilon*np.exp((self.p_cap-state[4])/self.epsilon))/(1+np.exp(self.p_cap-state[4]/self.epsilon))
+        self.dP = lambda H : (((1-self.p_base)/self.p_cap)*self.epsilon*np.exp((self.p_cap-H)/self.epsilon))/(1+np.exp(self.p_cap-H/self.epsilon))
 
+        self.Fix = self.FindFixpoint(self.MemoryIncrementForStability)
 
     def ClassicIncrement(self,t, state):
         """
@@ -63,7 +65,7 @@ class SIRModels:
         Return: 
             - state after one step
         """
-        P = self.P(state)
+        P = self.P(state[4])
         Gamma = self.Gamma(t)
         S , I, R, H1, H = state
 
@@ -86,7 +88,7 @@ class SIRModels:
         Return: 
             - state after one step
         """
-        P = self.P(state)
+        P = self.P(state[4])
         S , I, R, H1, H = state
 
         dS = -self.beta*P*1*I*S + self.nue*R
@@ -107,14 +109,14 @@ class SIRModels:
             - JacobiMatrix
         """
         Jac = np.zeros((4,4))
-        Jac[0,0] = - self.beta*self.P(state)*state[1]-self.nue
+        Jac[0,0] = - self.beta*self.P(state[4])*state[1]-self.nue
         Jac[0,1] = - self.beta*self.P(state)*state[0]-self.nue
 
-        Jac[0,3] = - self.beta*self.dP(state)*state[0]*state[1]
-        Jac[1,0] = self.beta*self.P(state)*state[1]
-        Jac[1,1] = self.beta*self.P(state)*state[0]-self.gamma
+        Jac[0,3] = - self.beta*self.dP(state[4])*state[0]*state[1]
+        Jac[1,0] = self.beta*self.P(state[4])*state[1]
+        Jac[1,1] = self.beta*self.P(state[4])*state[0]-self.gamma
 
-        Jac[1,3] = self.beta*self.dP(state)*state[0]*state[1]
+        Jac[1,3] = self.beta*self.dP(state[4])*state[0]*state[1]
 
         Jac[2,1] = 2/self.T
         Jac[2,2] = - 2/self.T
@@ -123,3 +125,26 @@ class SIRModels:
         Jac[3,2] = 2/self.T
         Jac[3,3] = - 2/self.T
         return Jac
+
+    def MemoryIncrementForStability(self, I):
+        """
+        The function returns the temporal derivative of the state vector 'state'=(S,I,R,H1,H) for the classical SIR Model.
+
+        Args:
+            - state   - Required: state=(S,I,R,H1,H) vector
+            - t    - Required: transmission rate
+
+        Return: 
+            - state after one step
+        """
+        return self.nue/(self.gamma+self.nue)*(1-self.gamma/(self.beta*self.P(I)))-I
+
+    def FindFixpoint(self,fun,I0 = 1):
+        Fix = opt.root(fun,I0)
+        I = Fix.x
+        S = self.gamma/(self.beta*self.P(I))
+        R = self.gamma/self.nue*I
+        H = I
+        H1 = I
+        Fix = np.array([S,I,R,H1,H])
+        return Fix
