767432ec4f4fca3e5d333d8f1ec119a23adbdd6e
simonblaue <sblaue@gmx.de>
2022-06-30 11:25:24 +0200

    added meomry and seasonality

--- ModelLib.py
+++ ModelLib.py
@@ -8,7 +8,16 @@ class SIRModels:
         self.beta = beta
         self.mue = mue
         self.nue = nue
-        
+        self.p_base = 0.1 # Strongest possible factor that reduces the transmission rate
+        self.p_cap = 1e-3 # percieved risk beyond which no further reduction of the reansmission rate takes palce
+        self.epsilon = 1e-4 # Curvature parameter
+        self.s = 0.5 # Amplitude of seasonal forcing
+        self.omega = 2*np.pi/360 #Frequency of yearly seasonsl variation
+        self.T = 100 # mittlerer Erinnerungszeit
+        self.P = lambda state: self.p_base+(1-self.p_base)/self.p_cap*self.epsilon*np.log(1+np.exp(1/self.epsilon*(self.p_cap-state[4])))
+        self.Gamma = lambda t : 1+self.s*np.cos(self.omega*t)
+
+    
     def ClassicIncrement(self,t, state):
         """
         The function returns the temporal derivative of the state vector 'state'=(S,I,R) for the classical SIR Model.
@@ -42,4 +51,40 @@ class SIRModels:
         return [dS,dI,dR]
 
 
+    def SeasonalyIncrement(self, t, state):
+        """
+        The function returns the temporal derivative of the state vector 'state'=(S,I,R,H1,H) for the classical SIR Model.
 
+        Args:
+            - state   - Required: state=(S,I,R,H1,H) vector
+            - t    - Required: transmission rate
+
+        Return: 
+            - state after one step
+        """
+        dS = -self.beta*self.P(state)*self.Gamma(t)*state[1]*state[0]+ self.nue*state[2]
+        dI = self.beta*self.P(state)*self.Gamma(t)+state[1]*state[0]- self.gamma+state[1]
+        dR = self.gamma*state[1]-self.nue+state[2]
+        dH1 = 2/self.T*(state[1]-state[3])
+        dH = 2/self.T*(state[3]-state[4])
+
+        return [dS,dI,dR,dH1,dH]
+
+    def MemoryIncrement(self, t, state):
+        """
+        The function returns the temporal derivative of the state vector 'state'=(S,I,R,H1,H) for the classical SIR Model.
+
+        Args:
+            - state   - Required: state=(S,I,R,H1,H) vector
+            - t    - Required: transmission rate
+
+        Return: 
+            - state after one step
+        """
+        dS = -self.beta*self.P(state)*1*state[1]*state[0]+ self.nue*state[2]
+        dI = self.beta*self.P(state)*1+state[1]*state[0]- self.gamma+state[1]
+        dR = self.gamma*state[1]-self.nue+state[2]
+        dH1 = 2/self.T*(state[1]-state[3])
+        dH = 2/self.T*(state[3]-state[4])
+
+        return [dS,dI,dR,dH1,dH]
